6fa77caaf9eb5ffefaaad9602b044678b8b624b9
Das203web-dev <shuvajitdas838@gmail.com>
2023-12-31 23:07:40 +0600

    site name change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ['https://assignment-11-client-a7acd.web.app'],
+    origin: ['drab-jewel.surge.sh'],
     credentials: true
 }))
 app.use(express.json())
@@ -36,12 +36,13 @@ const verifyToken = (req, res, next) => {
         if (err) {
             return res.status(401).send({ message: 'access denied' })
         }
+        console.log(token)
         req.user = decoded;
         next()
     })
 }
 const logger = (req, res, next) => {
-    console.log("log info", req.method, req.url);
+    // console.log("log info", req.method, req.url);
     next()
 }
 
@@ -56,6 +57,7 @@ async function run() {
 
         app.post("/jwt", async (req, res) => {
             const user = req.body;
+            console.log(user)
             const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '24h' });
             res.cookie('token', token, {
                 httpOnly: true,
@@ -132,6 +134,8 @@ async function run() {
             res.send(result)
         })
         app.get('/myBids', logger, verifyToken, async (req, res) => {
+            console.log(req.user.photoUrl)
+
             let query = {};
             if (req?.user?.email !== req?.query?.email) {
                 return res.status(401).send({ message: "Access deanied" })
